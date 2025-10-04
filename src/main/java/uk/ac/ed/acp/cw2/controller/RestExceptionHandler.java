2f9e6e3a759725f6e6eb6d92f5a962d05fef2488
gitluorid <gitluorid@users.noreply.github.com>
2025-10-05 00:34:18 +0100

    logging exceptions

--- src/main/java/uk/ac/ed/acp/cw2/controller/RestExceptionHandler.java
+++ src/main/java/uk/ac/ed/acp/cw2/controller/RestExceptionHandler.java
@@ -1,5 +1,7 @@
 package uk.ac.ed.acp.cw2.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -8,12 +10,15 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 /**
  * Global exception handler for REST controllers.
  * Catches malformed or unreadable JSON requests
- * an HTTP 400 (Bad Request) response
+ * and returns HTTP 400 (Bad Request) response
  */
 @RestControllerAdvice
 public class RestExceptionHandler {
+    private static final Logger logger = LoggerFactory.getLogger(RestExceptionHandler.class);
+
     @ExceptionHandler(HttpMessageNotReadableException.class)
-    public ResponseEntity<String> handleHttpMessageNotReadableException(HttpMessageNotReadableException ex) {
+    public ResponseEntity<String> handleHttpMessageNotReadableException(HttpMessageNotReadableException exception) {
+        logger.warn("Malformed JSON in request: {}", exception.getMessage());
         return ResponseEntity.badRequest().build();
     }
 }
